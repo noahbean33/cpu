24253ff2efdaa2dc0e87c3f75ec4d4dfb41cfabd
Noah Bean <92399044+noahbean33@users.noreply.github.com>
2026-02-17 17:53:17 -0800

    added back old drafts

--- src/cpu_v2/top.sv
+++ src/cpu_v2/top.sv
@@ -1,16 +1,30 @@
+`include "cpu.sv"
+`include "progmem.sv"
+module top(
+    input logic rst, clk,
+    output logic [31:0] cycle
+  );
+  logic [31:0] mem_rdata, mem_wdata, addr;
+  logic rstrb;
+  logic [3:0] wr_strobe;
+  
+  //Instantiate sub modules
+  cpu cpu0(
+        .rst(rst), .clk(clk),
+        .mem_rdata(mem_rdata),
+        .mem_addr(addr),
+        .cycle(cycle),
+        .mem_rstrb(rstrb),
+        .mem_wdata(mem_wdata),
+        .mem_wstrb(wr_strobe)
+      );
 
-// --------------------------------------------------------
-// Top-Level RISC-V Processor (Single-Cycle)
-// --------------------------------------------------------
-import risc_pkg::*;
-
-module top #(
-  parameter RESET_PC = 32'h0000
-)(
-  input logic clk,
-  input logic reset_n
-);
-
- // Enter your code
-
+  progmem mem0(
+            .rst(rst), .clk(clk),
+            .addr(addr),
+            .data_in(mem_wdata),
+            .rd_strobe(rstrb),
+            .wr_strobe(wr_strobe),
+            .data_out(mem_rdata)
+          );
 endmodule
---
+++ src/cpu_v3/top.sv
@@ -0,0 +1,16 @@
+
+// --------------------------------------------------------
+// Top-Level RISC-V Processor (Single-Cycle)
+// --------------------------------------------------------
+import risc_pkg::*;
+
+module top #(
+  parameter RESET_PC = 32'h0000
+)(
+  input logic clk,
+  input logic reset_n
+);
+
+ // Enter your code
+
+endmodule
